5a9daff79ec7ab817c4abcbe060371f9371b02f2
nicolecst <20228619@life.hkbu.edu.hk>
2023-12-27 11:57:35 +0800

    RegisterView Styling

--- itinerary/src/pages/RegisterView.vue
+++ itinerary/src/pages/RegisterView.vue
@@ -1,19 +1,22 @@
 <template>
-  <div>
-    <h1>Register Page</h1>
-    <form @submit.prevent= "register()">
-      <label>Username</label>
-      <input type="text" v-model="username" placeholder="username" />
+  <div class="bg">
+    <div class="loginForm">
+      <form @submit.prevent="register()">
+        <h1>Registration</h1>
+        <label>Username</label>
+        <input type="text" v-model="username" placeholder="username" />
 
-      <label>Email</label>
-      <input type="email" v-model="email" placeholder="email" />
+        <label>Email</label>
+        <input type="email" v-model="email" placeholder="email" />
 
-      <label>Password</label>
-      <input type="password" v-model="password" placeholder="password"/>
+        <label>Password</label>
+        <input type="password" v-model="password" placeholder="password" />
 
-      <button type="submit" class="btn btn-primary">Sign Up</button>
-    </form>
-    <p>{{users}}</p>
+        <button type="submit" class="btn btn-primary">Sign Up</button>
+      </form>
+    </div>
+
+    <p>{{ users }}</p>
   </div>
 </template>
 
@@ -26,9 +29,9 @@ export default {
   data() {
     return {
       users: [],
-      username: '',
-      email: '',
-      password: '',
+      username: "",
+      email: "",
+      password: "",
     };
   },
   async created() {
@@ -36,19 +39,75 @@ export default {
     const users = response.data;
     this.users = users;
   },
-  methods:{
-    async register(){
+  methods: {
+    async register() {
+      const response = await axios.post("/api/register", {
+        users: this.user,
+        username: this.username,
+        email: this.email,
+        password: this.password,
+      });
 
-        const response = await axios.post("/api/register", {
-            users: this.user,
-            username: this.username,
-            email: this.email,
-            password: this.password,
-        })
-
-        console.log(response)
-        this.$router.push('/login')
-    }
+      console.log(response);
+      this.$router.push("/login");
+    },
   },
 };
 </script>
+
+<style scoped>
+.bg {
+  background-color: #016a70;
+  background-image: url("@/assets/Images/City.jpg");
+  background-size: cover;
+  /* filter:brightness(70%); */
+  margin: auto;
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+}
+.btn {
+  background-color: #016a70;
+  border-color: #016a70;
+  color: #ffffdd;
+  --bs-btn-line-height: 1.3;
+  margin: 5%;
+}
+
+.btn:hover {
+  background-color: #ffffdd;
+  color: #016a70;
+}
+.loginForm {
+  width: 600px;
+  height: 400px;
+  background-color: #fff;
+  box-shadow: 0 0 20px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  display: flex;
+  overflow: hidden;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  /* margin-top: 30px; */
+}
+label {
+  display: block;
+  margin-left: 0;
+  text-align: left;
+  margin-top: 20px;
+  /* font-weight: 600; */
+}
+input {
+  display: block;
+  margin: 3% auto;
+  width: 100%;
+}
+p {
+  display: block;
+}
+h1 {
+  font-family: rabikiso;
+}
+</style>
