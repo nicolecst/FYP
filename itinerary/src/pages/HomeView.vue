7423765d2ebdfb20e4fb9d90bd55ad133c64bd0a
nicolecst <20228619@life.hkbu.edu.hk>
2024-03-13 15:03:36 +0800

    Search Filter by Actname

--- itinerary/src/pages/HomeView.vue
+++ itinerary/src/pages/HomeView.vue
@@ -1,42 +1,55 @@
 <template>
   <div>
     <NavBarVue />
+
     <div class="welcomeMsg">
-      <!-- <div>
-        <h1 style="display: block">Welcome {{ user.username }}!</h1>
-      </div> -->
-      <div>
-        <button class="btn create-btn" @click="create()">
+      <h1>Welcome {{ user.username }}!</h1>
+      <form class="d-flex align-items-center flex-nowrap search-form">
+        <input
+          type="text"
+          class="form-control input-lg"
+          placeholder="search by activity name.."
+          v-model="searchParam"
+          style="min-width: 80vw; margin-bottom: 50px;"
+        />
+        <!-- <button class="btn btn-outline-success">
+                <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+              </button> -->
+      </form>
+      <p>{{ searchParam }}</p>
+      <button class="btn create-btn" @click="create()">
           <span style="font-size: 1.5em">
-            <font-awesome-icon icon="fa-solid fa-calendar-plus" /> </span
-          >Create Itinerary
+            <font-awesome-icon icon="fa-solid fa-calendar-plus" style="margin-right: 8px;"/> </span
+          >Create Itinerary!
         </button>
-      </div>
     </div>
 
-    <div class="search-container">
-      <div class="search-inner-container">
-        <div class="row mt-2 justify-content-center align-items-center">
-          <div class="col-md-6">
-            <h1>Welcome {{ user.username }}!</h1>
-            <form class="d-flex align-items-center flex-nowrap search-form">
-              <input
-                type="text"
-                class="form-control form-input"
-                placeholder="search.."
-              />
-              <button class="btn btn-outline-success">
+    <!-- <div class="welcomMsg">
+      <div class="search-container">
+        <div class="search-inner-container">
+          <div class="row mt-2 justify-content-center align-items-center">
+            <div class="col-md-6">
+              <h1>Welcome {{ user.username }}!</h1>
+              <form class="d-flex align-items-center flex-nowrap search-form">
+                <input
+                  type="text"
+                  class="form-control form-input"
+                  placeholder="search by activity name.."
+                  v-model="searchParam"
+                />
+                <button class="btn btn-outline-success">
                 <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
               </button>
-            </form>
+              </form>
+            </div>
           </div>
+          <p>{{ searchParam }}</p>
         </div>
       </div>
-    </div>
-
+    </div> -->
     <div class="row" style="padding: 20px 20px 0px 20px">
       <div class="col-md-6">
-        <h3>All activities</h3>
+        <h3>Activities <span v-if="searchParam!=''">(Search Results)</span> </h3>
       </div>
       <div class="col-md-6" style="text-align: right">
         <a href="/activities">view all</a>
@@ -45,7 +58,7 @@
 
     <div class="scrolls">
       <CardViewVue
-        v-for="activity in approved().slice(0, 10)"
+        v-for="activity in filteredAct"
         :key="activity._id"
         :actname="activity.Act_name"
         :area="activity.Area"
@@ -57,6 +70,7 @@
         :img="activity.Image"
       />
     </div>
+    
     <h3>{{ user.preference1 }}</h3>
     <div class="scrolls">
       <CardViewVue
@@ -117,6 +131,7 @@ export default {
     return {
       activities: [],
       user: {},
+      searchParam: '',
     };
   },
   methods: {
@@ -129,6 +144,9 @@ export default {
     create() {
       this.$router.push("/create");
     },
+    // filterAct(param){
+    //   return this.activities.filter((a)=> a.Act_name === param && a.Approved === true)
+    // }
   },
   async created() {
     const response = await axios
@@ -147,6 +165,12 @@ export default {
       .get("/api/users/" + i)
       .then((response) => (this.user = response.data));
   },
+  computed: {
+    filteredAct: function () {
+      return this.activities.filter((a) => a.Act_name.toLowerCase().match(this.searchParam.toLowerCase()) && a.Approved === true);
+    }
+
+  },
 };
 </script>
 
@@ -162,37 +186,44 @@ a {
 }
 .welcomeMsg {
   display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-between;
-  margin: 10px;
+  justify-content: center;
+  margin: 0;
+  padding: 50px;
+  background-color: #0c3b2e;
+  color: #fff;
+  min-height: 50vh;
 }
 
 .search-container {
-  background-image: url("@/assets/Images/hong-kong-mountain-top-view.jpg");
+  /* background-image: url("@/assets/Images/hong-kong-mountain-top-view.jpg");
   background-position: center center;
-  background-size: cover;
+  background-size: cover; */
+  background-color: #0c3b2e;
   padding: 200px;
   justify-content: center;
   align-items: center;
 }
 
-.search-container::before{
-  background-color: rgba(0,0,0,0.25);
+.search-container::before {
+  background-color: rgba(0, 0, 0, 0.25);
 }
 
-.search-inner-container{
+.search-inner-container {
   background: white;
   padding: 30px;
   border-radius: 20px;
 }
 
 .create-btn {
-  border-color: #6D9773;
-  color: #6D9773;
+  border-color: #fff;
+  color: #fff;
 }
 .create-btn:hover {
-  border-color: #6D9773;
-  background-color: #6D9773;
+  border-color: #6d9773;
+  background-color: #6d9773;
   color: #ffffdd;
 }
 </style>
